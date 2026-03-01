bbafcf17150006f71c69c24db4e9e3dbb55663bc
Wissididom <30803034+Wissididom@users.noreply.github.com>
2026-03-01 16:12:29 +0100

    Update to .NET 10

--- Temperaturumrechner/Form2.vb
+++ Temperaturumrechner/Form2.vb
@@ -1,30 +1,36 @@
 ﻿Public Class Form2
 
     Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
-        Form1.Show()
+        Dim f1 As New Form1()
+        f1.Show()
     End Sub
 
     Private Sub Button2_Click(sender As System.Object, e As System.EventArgs) Handles Button2.Click
-        Form3.Show()
+        Dim f3 As New Form3()
+        f3.Show()
     End Sub
 
     Private Sub Button3_Click(sender As System.Object, e As System.EventArgs) Handles Button3.Click
-        Form4.show()
+        Dim f4 As New Form4()
+        f4.Show()
     End Sub
 
     Private Sub Button4_Click(sender As System.Object, e As System.EventArgs) Handles Button4.Click
-        Form5.show()
+        Dim f5 As New Form5()
+        f5.Show()
     End Sub
 
     Private Sub LinkLabel1_LinkClicked(sender As System.Object, e As System.Windows.Forms.LinkLabelLinkClickedEventArgs) Handles LinkLabel1.LinkClicked
-        System.Diagnostics.Process.Start("www.umrechnung.org/masseinheiten-temperatur-celsius-fahrenheit-kelvin/celsius-fahrenheit-umrechnung.htm")
+        Process.Start("www.umrechnung.org/masseinheiten-temperatur-celsius-fahrenheit-kelvin/celsius-fahrenheit-umrechnung.htm")
     End Sub
 
     Private Sub Button5_Click(sender As System.Object, e As System.EventArgs) Handles Button5.Click
-        Form6.show()
+        Dim f6 As New Form6()
+        f6.Show()
     End Sub
 
     Private Sub Button6_Click(sender As System.Object, e As System.EventArgs) Handles Button6.Click
-        Form7.Show()
+        Dim f7 As New Form1()
+        f7.Show()
     End Sub
 End Class
